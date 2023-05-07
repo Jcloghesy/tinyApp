0c68991c22f3dd3e04c02158a9df79e849e3c75b
John <john.cloghesy@gmail.com>
2023-05-06 19:13:49 -0600

    URL Shortening (#1): Add functionality generate a random short URL ID
    - Implement the generatRandomString function (6 chars long)
    - Add additional comments in server file - express_server.js

--- express_server.js
+++ express_server.js
@@ -7,12 +7,16 @@ const express = require("express");
 const app     =express();
 const PORT    = 8080;  /** default port 8080 */
 
+/** set view engine to ejs & add middleware/body-parse for POST requests */
 app.set( "view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(req.body); /** Log the POST request body to the console */
+  res.send("Ok"); /** [TODO]: Respond with 'Ok' (we will replace this) */
 });
+
+/** **** HTTP ROUTES **** */
 
 const urlDatabase = {
 	"b2xVn2":"http://www.lighthouselabs.ca",
@@ -51,6 +55,17 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
 });
+
+/** generate random string for short URL - string length to 6 chars */
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 app.get("/urls.json", (req,res) => {
 	res.json(urlDatabase);
