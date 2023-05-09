d73c42ba34339403e16aeee0958fd8f5330ad5ad
John <john.cloghesy@gmail.com>
2023-05-09 14:32:37 -0600

    Displaying Username with cookie-parser:  Pass username to views
      - Update server to pass-in username to views using _header.ejs partial

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app     =express();
 const PORT    = 8080;  /** default port 8080 */
 
@@ -20,6 +21,8 @@ app.listen(PORT, () => {
 app.set( "view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 /** ROUTES  */
 
@@ -38,19 +41,27 @@ app.get("/hello",(req,res)=>{
 
 /** Responds to /url Get rsquest with rendered HTML of urls_index.ejs */
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+    const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
 /** Responds to /url Get request with rendered HTML of urls_new.ejs */
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
