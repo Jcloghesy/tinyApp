01a9392412daa2ebedffe2ebc3ed8a0e8d61efd7
John <john.cloghesy@gmail.com>
2023-05-25 00:18:12 -0600

    Submit Project for Evaluation: Update helper.js - comments & .gitignore
       - Helper.js       - add comment to each function, refactor to better readability
       - .gitignore      - update to disregard directory for old files & setup files for VS Code plugins

--- helpers.js
+++ helpers.js
@@ -1,32 +1,39 @@
-/** ** MODULE TO HOLD  FUNCTIONS REFERENCED SEVERAL TIMES - helpers.js ** */
+/** ** MODULE TO HOLD HELPER FUNCTIONS  - helpers.js ** */
 
-const generateRandomString = (length) => {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+
+/** Function generates new shortURls (6 characters in length) */
+const generateRandomString = function() {
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) { 
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 };
 
-const getUserByEmail = function (email, users) {
+
+/** Finds a user by email in the users object otherwise null  */
+function getUserByEmail(email, users) {
   for (const userId in users) {
     if (users[userId].email === email) {
       return users[userId];
     }
   }
-  return undefined;
+  return null;
 };
 
-const urlsForUser = (id, urlDatabase) => {
-  let userUrls = {};
-  for (let i in urlDatabase) {
-    if (urlDatabase[i].userID === id) {
-      userUrls[i] = urlDatabase[i];
+
+/** Filters and returns object with URLs associated to user ID */
+function urlsForUser(id, urlDatabase) {
+  const filteredURL = {};
+  for (const urlId in urlDatabase) {
+    if (urlDatabase[urlId].userId === id) {
+      filteredURL[urlId] = urlDatabase[urlId];
     }
   }
-  return userUrls;
-};
+  return filteredURL;
+}
 
+/** Export helper functions */
 module.exports = { generateRandomString, getUserByEmail, urlsForUser };
